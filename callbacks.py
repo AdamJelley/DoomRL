7f47dae2760a5b2917db26f1d8401f57c247c547
Adam Jelley <47476156+AdamJelley@users.noreply.github.com>
2021-07-09 16:32:13 +0100

    Include model checkpointing

--- callbacks.py
+++ callbacks.py
@@ -5,19 +5,22 @@ class myTensorBoardCallback(BaseCallback):
     Custom callback for plotting additional values in tensorboard
     """
 
-    def __init__(self, verbose=1):
+    def __init__(self, avg_reward=True, verbose=1):
         super().__init__(verbose)
+        self.avg_reward = avg_reward
         self.total_reward=0
         self.episode_length=0
 
     def _on_step(self):
-        unscaled_reward = self.training_env.get_original_reward()[0]
-        self.total_reward+=unscaled_reward
-        self.episode_length+=1
+        if self.avg_reward:
+            unscaled_reward = self.training_env.get_original_reward()[0]
+            self.total_reward+=unscaled_reward
+            self.episode_length+=1
         return True
 
     def _on_rollout_end(self):
-        self.average_policy_return = self.total_reward/self.episode_length
-        self.logger.record('train/AvgStepReturn', self.average_policy_return)
+        if self.avg_reward:
+            self.average_policy_return = self.total_reward/self.episode_length
+            self.logger.record('train/AvgReward', self.average_policy_return)
         self.total_reward=0
         self.episode_length=0
--- train_agent.py
+++ train_agent.py
@@ -1,17 +1,23 @@
 from stable_baselines3 import PPO
 from preprocessing import create_env
 from callbacks import myTensorBoardCallback
+from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
 from stable_baselines3.common.evaluation import evaluate_policy
 
-saved_model_name = 'PPO_Basic'
+model_name = 'PPO_Basic'
 env_name = 'VizdoomBasic-v0'
 
 env = create_env(env_name)
 
-model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=f"./tensorboard/{env_name}/{saved_model_name}/")
+model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=f"./tensorboard/{env_name}/{model_name}/")
 
-model.learn(total_timesteps=50000, callback=myTensorBoardCallback())
-model.save('./saved_models/'+str(saved_model_name))
+eval_callback = EvalCallback(env, best_model_save_path=f"./saved_models/{env_name}/{model_name}", \
+    eval_freq=10000, n_eval_episodes=5, deterministic=True, render=False)
+
+myTensorBoardCallback = myTensorBoardCallback(avg_reward=True)
+
+model.learn(total_timesteps=50000, callback=[eval_callback, myTensorBoardCallback])
+model.save(f"./saved_models/{env_name}/{model_name}")
 
 mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 print(f"Mean Reward: {mean_reward}, Std Reward:{std_reward}")
